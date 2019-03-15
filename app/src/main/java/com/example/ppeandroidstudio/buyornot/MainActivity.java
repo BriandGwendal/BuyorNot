ab8d05b4fdc8576fb270a49f6a22de9243ba134f
gwendalB <gwendal.briand1999@gmail.com>
2019-03-15 16:58:42 +0100

    Design

--- app/src/main/java/com/example/ppeandroidstudio/buyornot/MainActivity.java
+++ app/src/main/java/com/example/ppeandroidstudio/buyornot/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ppeandroidstudio.buyornot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /*private void hideSystemUI() {
+
+        View activity_main = getWindow().getDecorView();
+        activity_main.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }*/
+    public void afficherAjouter(View view){
+        Intent intent = new Intent(this, AjouterActivity.class);
+        Bundle bundle = new Bundle();
+        intent.putExtras(bundle);
+        startActivity(intent);
     }
 }
