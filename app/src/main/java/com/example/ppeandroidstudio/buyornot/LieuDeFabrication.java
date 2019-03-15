c1ae46da7a1accdeb1a431d5ed52b797a4a0c8f6
leneveuT <leneveu.thibaut@gmail.com>
2019-03-15 16:08:10 +0100

    Lieux_De_Fabrication -> LieuDeFabrication

--- app/src/main/java/com/example/ppeandroidstudio/buyornot/LieuDeFabrication.java
+++ app/src/main/java/com/example/ppeandroidstudio/buyornot/LieuDeFabrication.java
@@ -1,14 +1,14 @@
 package com.example.ppeandroidstudio.buyornot;
 
-public class Lieux_De_Fabrication {
+public class LieuDeFabrication {
     private int id_lieux_De_Fabrication;
     private String code_lieux_De_Fabrication;
     private String libelle_lieux_De_Fabrication;
 
-    public Lieux_De_Fabrication() {
+    public LieuDeFabrication() {
     }
 
-    public Lieux_De_Fabrication(int id_lieux_De_Fabrication, String code_lieux_De_Fabrication, String libelle_lieux_De_Fabrication) {
+    public LieuDeFabrication(int id_lieux_De_Fabrication, String code_lieux_De_Fabrication, String libelle_lieux_De_Fabrication) {
         this.id_lieux_De_Fabrication = id_lieux_De_Fabrication;
         this.code_lieux_De_Fabrication = code_lieux_De_Fabrication;
         this.libelle_lieux_De_Fabrication = libelle_lieux_De_Fabrication;
